b0e4c2d5eeb35bb714352e82e24da41b34aeb866
Ramy <Ramy@Ramys-iMac.local>
2020-05-08 15:31:35 -0400

    Add pre and code syles

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,10 @@ export default function Home() {
       <section className={utilStyles.headingMd}>
         <p>[Your Self Introduction]</p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{" "}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+          (This is a sample website - you’ll be building a site like this on <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
+        <p>And this is my new paragraaaaaph!!!! Sick</p>
+        <pre>npm install</pre>
         <a href="/page2">Go to page 2</a>
       </section>
     </Layout>
