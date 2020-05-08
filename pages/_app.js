19161066470f988ab870b9e0fe0ba125d7f8cbef
Ramy <Ramy@Ramys-iMac.local>
2020-05-08 15:16:07 -0400

    Initial commit

---
+++ pages/_app.js
@@ -0,0 +1,6 @@
+import '../styles/global.sass'
+import '../styles/fonts.sass'
+
+export default function App({ Component, pageProps }) {
+    return <Component {...pageProps} />
+}
--- pages/page2.js
+++ pages/page2.js
@@ -6,15 +6,13 @@ export default function Home() {
   return (
     <Layout home>
       <Head>
-        <title>{siteTitle}</title>
+        <title>Page 2 — {siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{" "}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
-        <a href="/page2">Go to page 2</a>
+        <div>
+        </div>
+        <p>Another page!</p>
+        <a href="/">Go Back Home!</a>
       </section>
     </Layout>
   );
