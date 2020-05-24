83dc1cde4970e9cf05b647cded057be664eb9e74
Ramy Majouji <ramy@majouji.me>
2020-05-23 21:09:24 -0400

    new

---
+++ components/embeds/apple-music.js
@@ -0,0 +1,13 @@
+export default function AppleMusicEmbed({ url }) {
+  return (
+    <iframe
+      className="embed--apple-music"
+      allow="autoplay *; encrypted-media *;"
+      frameborder="0"
+      height="450px"
+      maxHeight="100%"
+      sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation"
+      src={`${url}`}
+    ></iframe>
+  );
+}
